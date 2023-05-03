228194dc8564fc8bf5f1ca0d959246cdfa915c22
Chandan Majhi <100117918+cmhightech@users.noreply.github.com>
2023-05-03 13:57:00 +0500

    Add files via upload

---
+++ sound-board/script.js
@@ -0,0 +1,25 @@
+const sounds = ['Odia song', 'English song', 'Deshbhakti song', 'Motivational song', 'Bhajan song', 'Relaxation song','Hindi song']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
